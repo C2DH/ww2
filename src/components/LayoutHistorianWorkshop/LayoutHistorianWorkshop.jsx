406935f76f6cc9a574f23400d715117f52b05f6a
Yoann Bettinelli <yoann@bunkerpalace.com>
2024-08-22 14:38:01 +0200

    fix use dvh historian workshop

--- src/components/LayoutHistorianWorkshop/LayoutHistorianWorkshop.jsx
+++ src/components/LayoutHistorianWorkshop/LayoutHistorianWorkshop.jsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion'
 export default function LayoutHistorianWorkshop({ children, pageTitle }) {
     return (
         <motion.div style={{ backgroundImage: `url(${bgPaper})`}} className='w-full h-[calc(100vh-120px)] overflow-scroll' exit={{opacity: 0.999, transition: {duration: siteConfig.curtainsTransitionDuration}}}>
-            <div className="container mx-auto h-[calc(100dvh-120px)] min-h-[calc(100vh-120px)] sm:h-[calc(100vh-120px)] flex flex-col px-[20px] xl:px-0">
+            <div className="container mx-auto h-[calc(100dvh-120px)] sm:min-h-[calc(100vh-120px)] sm:h-[calc(100vh-120px)] flex flex-col px-[20px] xl:px-0">
                 
                 <div className='lg:hidden pt-[30px]'>
                     <h1 className='text-[40px] font-abril leading-none'>{pageTitle}</h1>
