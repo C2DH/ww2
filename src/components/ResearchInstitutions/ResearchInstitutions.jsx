0de2d00abffc7809d32b123b0c22289c80f084c9
Daniele Guido <gui.daniele@gmail.com>
2025-02-04 10:33:09 +0100

    fix #25 show more institutions

--- src/components/ResearchInstitutions/ResearchInstitutions.jsx
+++ src/components/ResearchInstitutions/ResearchInstitutions.jsx
@@ -11,83 +11,92 @@ import { useMenuHistorianContext } from '../../contexts/MenuHistorianProvider'
 import { fetchData } from '../../lib/utils'
 
 export default function ResearchInstitutions() {
+  const [sharedState, setSharedState] = useSharedState()
+  const { t } = useTranslation()
+  const [isOpenMenu, setIsOpenMenu] = useState(false)
+  const { pathname } = useLocation()
+  const menuItems = useMenuHistorianContext()
+  const [results, setResults] = useState([])
 
-    const [sharedState, setSharedState] = useSharedState()
-    const { t } = useTranslation()
-    const [isOpenMenu, setIsOpenMenu] = useState(false)
-    const { pathname } = useLocation()
-    const menuItems = useMenuHistorianContext()
-    const [results, setResults] = useState([])
-
-
-    const fetchInstitutions = async () => {
-        try {
-            const params = { data__type : "institution" }
-            const data = await fetchData('document', params)
-            return data ? data.results : []
-        } catch (error) {
-            console.error('Erreur lors de la récupération des documents :', error)
-            return []
-        }
+  const fetchInstitutions = async () => {
+    try {
+      const params = { data__type: 'institution' }
+      const data = await fetchData('document', params, 100)
+      return data ? data.results : []
+    } catch (error) {
+      console.error('Erreur lors de la récupération des documents :', error)
+      return []
     }
+  }
 
-    const getInstitutions = async () => {
-        const institutions = await fetchInstitutions()
-        setResults(institutions)
-    }
+  const getInstitutions = async () => {
+    const institutions = await fetchInstitutions()
+    setResults(institutions)
+  }
 
-    useEffect(() => {
-        getInstitutions()
-    }, [])
+  useEffect(() => {
+    getInstitutions()
+  }, [])
 
+  useEffect(() => {
+    setSharedState({ ...sharedState, showClouds: false, showCurtains: false })
+  }, [])
 
-    useEffect(() => {
-        setSharedState({ ...sharedState, showClouds: false, showCurtains: false })
-    }, [])
+  return (
+    <LayoutHistorianWorkshop pageTitle={t('menuItems.research_institutions')}>
+      <HeaderHistorianWorkshop items={menuItems} />
 
-    return (
-        
-        <LayoutHistorianWorkshop pageTitle={ t('menuItems.research_institutions')}>
+      {/** Content */}
+      <div className='lg:overflow-scroll'>
+        <div className='grid grid-cols-12 gap-[20px] pt-[40px] pb-[100px] lg:pb-[40px]'>
+          {results?.map((item, index) => {
+            return <CardLink key={index} data={item} />
+          })}
+        </div>
+      </div>
 
-            <HeaderHistorianWorkshop items={ menuItems } />
-            
-            {/** Content */}
-            <div className="lg:overflow-scroll">
-                <div className="grid grid-cols-12 gap-[20px] pt-[40px] pb-[100px] lg:pb-[40px]">
-                    { results?.map((item, index) => {
-                        return (
-                            <CardLink key={index} data={ item }/>
-                        )
-                    })}
-                </div>
-            </div>
+      {/* MOBILE: BTN MENU */}
+      <div
+        className='lg:hidden fixed bottom-0 left-0 right-0 z-[100] h-[70px] w-full flex border-t border-black'
+        style={{
+          backgroundImage: `url(${bgPaper})`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        <div
+          onClick={() => setIsOpenMenu(!isOpenMenu)}
+          className='flex items-center justify-center w-full'
+        >
+          <span className='uppercase text-[24px] cursor-pointer'>Menu</span>
+        </div>
+      </div>
 
-            {/* MOBILE: BTN MENU */}
-            <div className='lg:hidden fixed bottom-0 left-0 right-0 z-[100] h-[70px] w-full flex border-t border-black' style={{ backgroundImage: `url(${bgPaper})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
-                <div 
-                    onClick={() => setIsOpenMenu(!isOpenMenu)}
-                    className="flex items-center justify-center w-full"
-                >
-                    <span className='uppercase text-[24px] cursor-pointer'>Menu</span>
-                </div>
-            </div>
-
-            {/* MOBILE: MENU */}
-            <div className={classNames('lg:hidden h-[360px] fixed bottom-[70px] left-0 right-0 bg-paper border-black border-t transition-all duration-[750ms]', {
-                "translate-y-[100%]": !isOpenMenu
-            })}>
-                <ul className='text-[38px] uppercase flex flex-col justify-center items-center h-full gap-4'>
-                    {menuItems.map((item, index) => 
-                        <li key={index}>
-                            <Link key={index} to={item.link} className={classNames('navbar-title', {'active' : pathname === `${item.link}`})}>{item.title}</Link>
-                        </li>
-                   )}
-                </ul>
-            </div>
-            
-        </LayoutHistorianWorkshop>
-    )
+      {/* MOBILE: MENU */}
+      <div
+        className={classNames(
+          'lg:hidden h-[360px] fixed bottom-[70px] left-0 right-0 bg-paper border-black border-t transition-all duration-[750ms]',
+          {
+            'translate-y-[100%]': !isOpenMenu,
+          }
+        )}
+      >
+        <ul className='text-[38px] uppercase flex flex-col justify-center items-center h-full gap-4'>
+          {menuItems.map((item, index) => (
+            <li key={index}>
+              <Link
+                key={index}
+                to={item.link}
+                className={classNames('navbar-title', {
+                  active: pathname === `${item.link}`,
+                })}
+              >
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </LayoutHistorianWorkshop>
+  )
 }
-
-
-
